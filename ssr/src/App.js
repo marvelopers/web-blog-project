b4bba338951e44a2fc21fca6cbc4a3001a235c79
marvelopers <marvelopers@gmail.com>
2021-01-05 11:02:26 +0900

    Move 레거시 코드

---
+++ ssr/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { createGlobalStyle } from 'styled-components';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
